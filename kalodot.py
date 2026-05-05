30375b624a5c9df8cc7514f109e01f7b5b4916a8
Alkim <alkim@192.168.1.106>
2026-05-05 21:01:19 +0300

    Switched to an older model

--- kalodot.py
+++ kalodot.py
@@ -1,5 +1,9 @@
 from app import app
 
+@app.route('/')
+def home():
+    return "Kalodot Backend is LIVE and Running!", 200
+
 if __name__ == '__main__':
     # Threaded=True allows multiple Godot requests to happen at once
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
